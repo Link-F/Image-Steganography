ea033e6facea4328c5c8f72958114b630d7326ac
Florian Link <Florian.Link.1@gmx.net>
2017-08-13 16:00:52 +0200

    renamed gui elements into english

--- Image-Steganography/Form1.Designer.cs
+++ Image-Steganography/Form1.Designer.cs
@@ -78,9 +78,9 @@
             this.verstecken.Location = new System.Drawing.Point(23, 388);
             this.verstecken.Margin = new System.Windows.Forms.Padding(2);
             this.verstecken.Name = "verstecken";
-            this.verstecken.Size = new System.Drawing.Size(77, 27);
+            this.verstecken.Size = new System.Drawing.Size(92, 27);
             this.verstecken.TabIndex = 0;
-            this.verstecken.Text = "Einbinden";
+            this.verstecken.Text = "Integrate string";
             this.verstecken.UseVisualStyleBackColor = true;
             this.verstecken.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -188,10 +188,10 @@
             this.radioButton1.AutoSize = true;
             this.radioButton1.Location = new System.Drawing.Point(233, 27);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(73, 17);
+            this.radioButton1.Size = new System.Drawing.Size(74, 17);
             this.radioButton1.TabIndex = 12;
             this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Bitebene1";
+            this.radioButton1.Text = "bit-plane 1";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // radioButton2
@@ -199,10 +199,10 @@
             this.radioButton2.AutoSize = true;
             this.radioButton2.Location = new System.Drawing.Point(233, 50);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(73, 17);
+            this.radioButton2.Size = new System.Drawing.Size(74, 17);
             this.radioButton2.TabIndex = 13;
             this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Bitebene2";
+            this.radioButton2.Text = "bit-plane 2";
             this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // radioButton3
@@ -210,10 +210,10 @@
             this.radioButton3.AutoSize = true;
             this.radioButton3.Location = new System.Drawing.Point(233, 73);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(73, 17);
+            this.radioButton3.Size = new System.Drawing.Size(74, 17);
             this.radioButton3.TabIndex = 14;
             this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Bitebene3";
+            this.radioButton3.Text = "bit-plane 3";
             this.radioButton3.UseVisualStyleBackColor = true;
             // 
             // radioButton4
@@ -221,10 +221,10 @@
             this.radioButton4.AutoSize = true;
             this.radioButton4.Location = new System.Drawing.Point(233, 96);
             this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(73, 17);
+            this.radioButton4.Size = new System.Drawing.Size(74, 17);
             this.radioButton4.TabIndex = 15;
             this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "Bitebene4";
+            this.radioButton4.Text = "bit-plane 4";
             this.radioButton4.UseVisualStyleBackColor = true;
             // 
             // radioButton5
@@ -232,10 +232,10 @@
             this.radioButton5.AutoSize = true;
             this.radioButton5.Location = new System.Drawing.Point(233, 119);
             this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(73, 17);
+            this.radioButton5.Size = new System.Drawing.Size(74, 17);
             this.radioButton5.TabIndex = 16;
             this.radioButton5.TabStop = true;
-            this.radioButton5.Text = "Bitebene5";
+            this.radioButton5.Text = "bit-plane 5";
             this.radioButton5.UseVisualStyleBackColor = true;
             // 
             // radioButton6
@@ -243,10 +243,10 @@
             this.radioButton6.AutoSize = true;
             this.radioButton6.Location = new System.Drawing.Point(233, 142);
             this.radioButton6.Name = "radioButton6";
-            this.radioButton6.Size = new System.Drawing.Size(73, 17);
+            this.radioButton6.Size = new System.Drawing.Size(74, 17);
             this.radioButton6.TabIndex = 17;
             this.radioButton6.TabStop = true;
-            this.radioButton6.Text = "Bitebene6";
+            this.radioButton6.Text = "bit-plane 6";
             this.radioButton6.UseVisualStyleBackColor = true;
             // 
             // radioButton7
@@ -254,10 +254,10 @@
             this.radioButton7.AutoSize = true;
             this.radioButton7.Location = new System.Drawing.Point(233, 165);
             this.radioButton7.Name = "radioButton7";
-            this.radioButton7.Size = new System.Drawing.Size(73, 17);
+            this.radioButton7.Size = new System.Drawing.Size(74, 17);
             this.radioButton7.TabIndex = 18;
             this.radioButton7.TabStop = true;
-            this.radioButton7.Text = "Bitebene7";
+            this.radioButton7.Text = "bit-plane 7";
             this.radioButton7.UseVisualStyleBackColor = true;
             // 
             // radioButton8
@@ -265,10 +265,10 @@
             this.radioButton8.AutoSize = true;
             this.radioButton8.Location = new System.Drawing.Point(233, 188);
             this.radioButton8.Name = "radioButton8";
-            this.radioButton8.Size = new System.Drawing.Size(73, 17);
+            this.radioButton8.Size = new System.Drawing.Size(74, 17);
             this.radioButton8.TabIndex = 19;
             this.radioButton8.TabStop = true;
-            this.radioButton8.Text = "Bitebene8";
+            this.radioButton8.Text = "bit-plane 8";
             this.radioButton8.UseVisualStyleBackColor = true;
             // 
             // pictureBox3
@@ -290,7 +290,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(77, 27);
             this.button1.TabIndex = 21;
-            this.button1.Text = "Speichern";
+            this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
@@ -299,9 +299,9 @@
             this.button2.Location = new System.Drawing.Point(23, 3);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(77, 27);
+            this.button2.Size = new System.Drawing.Size(92, 27);
             this.button2.TabIndex = 22;
-            this.button2.Text = "Bitebenen";
+            this.button2.Text = "Show bit-planes";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -310,9 +310,9 @@
             this.button4.Location = new System.Drawing.Point(327, 3);
             this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(77, 27);
+            this.button4.Size = new System.Drawing.Size(93, 27);
             this.button4.TabIndex = 24;
-            this.button4.Text = "Bitebenen";
+            this.button4.Text = "Show bit-planes";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
@@ -323,7 +323,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(77, 27);
             this.button3.TabIndex = 25;
-            this.button3.Text = "Speichern";
+            this.button3.Text = "Save";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button5
@@ -349,9 +349,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(20, 293);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.Size = new System.Drawing.Size(49, 13);
             this.label1.TabIndex = 28;
-            this.label1.Text = " Schriftgröße";
+            this.label1.Text = "Font size";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
@@ -399,11 +399,11 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.label5.Location = new System.Drawing.Point(23, 222);
+            this.label5.Location = new System.Drawing.Point(20, 222);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(192, 18);
+            this.label5.Size = new System.Drawing.Size(46, 18);
             this.label5.TabIndex = 34;
-            this.label5.Text = "Einzubindende Zeichenkette";
+            this.label5.Text = "String";
             // 
             // Form1
             // 
